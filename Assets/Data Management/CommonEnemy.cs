c60a4d98ff4804d2c81ee0aad5dded3bbaad715e
Michael Rodyushkin <61915029+PotentialError@users.noreply.github.com>
2021-01-26 20:43:04 -0500

    Added CommonEnemy Class

---
+++ Assets/Data Management/CommonEnemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommonEnemy
+{
+    public string name { get; private set; }
+    public int low_damage { get; private set; }
+    public int high_damage { get; private set; }
+    public CommonEnemy(string id)
+    {
+        string conn = "URI=file:" + Application.dataPath + "/data.db"; //Path to database.
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string sqlQuery = "SELECT enemy_name, low_damage, high_damage " + "FROM common_enemies";
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+        while (reader.Read())
+        {
+            string name = reader.GetString(0);
+            int low_damage = reader.GetInt32(1);
+            string high_damage = reader.GetInt32(2);
+
+            Debug.Log("name = " + name + "  low_damage = " + damage + "  high_damage = " + high_damage);
+        }
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+    }
+}
