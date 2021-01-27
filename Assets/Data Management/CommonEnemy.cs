373337e650ddee229f50686154775e00fcb41956
Michael Rodyushkin <61915029+PotentialError@users.noreply.github.com>
2021-01-26 21:16:15 -0500

    Added TestClass object and successfully implemeted CommonEnemy

--- Assets/Data Management/CommonEnemy.cs
+++ Assets/Data Management/CommonEnemy.cs
@@ -1,29 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
+using System;
 
 public class CommonEnemy
 {
+    /*
+     Attributes of Common Enemy:
+        name : string
+        low_damage : int
+        high_damage : int
+     */
     public string name { get; private set; }
     public int low_damage { get; private set; }
     public int high_damage { get; private set; }
     public CommonEnemy(string id)
     {
-        string conn = "URI=file:" + Application.dataPath + "/data.db"; //Path to database.
+        string conn = "URI=file:" + Application.dataPath + "/Data Management/data.db"; //Path to database.
         IDbConnection dbconn;
         dbconn = (IDbConnection)new SqliteConnection(conn);
         dbconn.Open(); //Open connection to the database.
         IDbCommand dbcmd = dbconn.CreateCommand();
-        string sqlQuery = "SELECT enemy_name, low_damage, high_damage " + "FROM common_enemies";
+        string sqlQuery = "SELECT enemy_name, low_damage, high_damage " + "FROM common_enemies" + " WHERE ID = \"" + id + "\";";
         dbcmd.CommandText = sqlQuery;
         IDataReader reader = dbcmd.ExecuteReader();
         while (reader.Read())
         {
-            string name = reader.GetString(0);
-            int low_damage = reader.GetInt32(1);
-            string high_damage = reader.GetInt32(2);
+            name = reader.GetString(0);
+            low_damage = reader.GetInt32(1);
+            high_damage = reader.GetInt32(2);
 
-            Debug.Log("name = " + name + "  low_damage = " + damage + "  high_damage = " + high_damage);
+            Debug.Log("name = " + name + "  low_damage = " + low_damage + "  high_damage = " + high_damage);
         }
         reader.Close();
         reader = null;
---
+++ Assets/TestClasses.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestClasses : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CommonEnemy enemy = new CommonEnemy("chromebook_n22");
+        Debug.Log(enemy.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
