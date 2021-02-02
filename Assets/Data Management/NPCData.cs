ced114e17105a3deeadcdc92f02ec6eed3131c17
Michael Rodyushkin <61915029+PotentialError@users.noreply.github.com>
2021-02-02 16:26:33 -0500

    Added NPC, Playables, and Quest Classes | Completed QuestClass Outline

--- Assets/Data Management/NPCData.cs
+++ Assets/Data Management/NPCData.cs
@@ -2,15 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestClasses : MonoBehaviour
+public class NPC : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        //CommonEnemyData enemy = new CommonEnemyData("chromebook_n22");
-        //Debug.Log(enemy.name);
-        QuestData quest = new QuestData("getting_started");
-        Debug.Log(quest.description);
+        
     }
 
     // Update is called once per frame
--- Assets/Data Management/QuestData.cs
+++ Assets/Data Management/QuestData.cs
@@ -5,34 +5,34 @@ using Mono.Data.Sqlite;
 using System.Data;
 using System;
 
-public class CommonEnemy
+public class QuestData
 {
     /*
-     Attributes of Common Enemy:
+     Attributes of Quest:
         name : string
-        low_damage : int
-        high_damage : int
+        description : string
+        xp_gained : int
      */
     public string name { get; private set; }
-    public int low_damage { get; private set; }
-    public int high_damage { get; private set; }
-    public CommonEnemy(string id)
+    public string description { get; private set; }
+    public int xp_gained { get; private set; }
+    public QuestData(string id)
     {
         string conn = "URI=file:" + Application.dataPath + "/Data Management/data.db"; //Path to database.
         IDbConnection dbconn;
         dbconn = (IDbConnection)new SqliteConnection(conn);
         dbconn.Open(); //Open connection to the database.
         IDbCommand dbcmd = dbconn.CreateCommand();
-        string sqlQuery = "SELECT enemy_name, low_damage, high_damage " + "FROM common_enemies" + " WHERE ID = \"" + id + "\";";
+        string sqlQuery = "SELECT quest_name, quest_description, xp_gained " + "FROM quests" + " WHERE ID = \"" + id + "\";";
         dbcmd.CommandText = sqlQuery;
         IDataReader reader = dbcmd.ExecuteReader();
         while (reader.Read())
         {
             name = reader.GetString(0);
-            low_damage = reader.GetInt32(1);
-            high_damage = reader.GetInt32(2);
+            description = reader.GetString(1);
+            xp_gained = reader.GetInt32(2);
 
-            Debug.Log("name = " + name + "  low_damage = " + low_damage + "  high_damage = " + high_damage);
+            Debug.Log("name = " + name + "  description = " + description + "  xp_gained = " + xp_gained);
         }
         reader.Close();
         reader = null;
