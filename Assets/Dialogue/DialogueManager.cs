8a264ea8206e86a66589451dcce7634963f0f07a
Michael Rodyushkin <61915029+PotentialError@users.noreply.github.com>
2021-02-22 19:09:40 -0500

    Created Sample text box with animation, text progression, and name shown

---
+++ Assets/Dialogue/DialogueManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+    private Queue<string> sentences;
+    public Animator anim;
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+    public void StartDialogue(Dialogue dialogue)
+    {
+        sentences.Clear();
+        nameText.text = dialogue.name;
+        anim.SetBool("opening", true);
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSetence();
+    }
+    public void DisplayNextSetence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentences.Dequeue()));
+    }
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+    public void EndDialogue()
+    {
+        anim.SetBool("opening", false);
+    }
+}
