8a3a24192b7f71025a11af833954bdb6816c0451
Chris <chris.adonizio@gmail.com>
2021-02-25 19:23:05 -0500

    Added Combat Enemy Data Class

---
+++ Assets/Data Management/CombatEnemyData.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CombatEnemyData
+{
+    public static int enemyPartySize = 4;
+    public static bool isBossFight = false;
+    public static CommonCombatEnemy[] commonCombatEnemyParty = new CommonCombatEnemy[enemyPartySize];
+    public static BossData boss { get; private set; }
+    public static int bossCurrentHealth { get; private set; }
+
+    //Static method that sets up the boss for the fight. 
+    public static void setCombatEnemy(bool isBoss, string id)
+    {
+        isBossFight = isBoss;
+        if(isBoss)
+        {
+            // boss = new BossData(id);
+            // bossCurrentHealth = boss.health;
+        }
+        else
+        {
+
+        }
+    }
+
+    public class CommonCombatEnemy {
+        public CommonEnemyData combatEnemyData;
+        public int currentHealth;
+        public CommonCombatEnemy(string id)
+        {
+            // combatEnemyData = new CommonEnemyData(id);
+            // currentHealth = combatEnemyData.health;
+        }
+    }
+}
